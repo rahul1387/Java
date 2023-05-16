a6c1ba6a38d6eceb7b22682e2618c4bfd3e6a4c2
rahul1387 <rahultripathi7447@gmail.com>
2023-05-16 14:15:23 +0530

    Data Types

---
+++ src/datatypes.java
@@ -0,0 +1,28 @@
+public class datatypes {
+    public static void main(String[] args) {
+        //Data types sre specify the different size and values that can be stored in the variables.
+
+        //Data Types are of two types---> Primitive And Non-Primitive data types
+
+        /*
+        Primitive Data types---> 1. Int: this is of 4 byte and the range is from -2,147,483,648 to 2,147,483,647.
+                                 2. Short: This is of 2 byte and the range is from -32,768 to 32,767.
+                                 3. Long (L): This is of 6 byte and the range from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807.
+                                 4. Byte: This is of 1 byte and the range is from -128 to 127.
+                                 5. Float(F): This is of 4 byte and the range is beyond to understand.
+                                 6. Double (We can use D but not compulsory): This is of 6 byte and the range is beyond to understand.
+                                 7. Char (''): This is of 4 byte, and it is support by Unicode.
+                                 8. boolean: This will return value of boolean type like true or false.
+            Here some Example:
+         */
+        byte num6 = 12;
+        int num1 = 123456;
+        short num3 = 333;
+        long num2 = 1595784512369L;
+        float num4 = 1.1F;
+        double num5 = 1.12D;
+        char uni = 'A';
+        boolean yn = true;
+
+    }
+}
