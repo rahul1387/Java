4e5e7e2eed46e3da004719553e9a48275e412800
rahul1387 <rahultripathi7447@gmail.com>
2023-05-17 14:04:51 +0530

    Operators in Java

---
+++ src/operators.java
@@ -0,0 +1,24 @@
+public class operators {
+    public static void main(String[] args) {
+        //Operators: operators are symbol that perform operations on data, such as number or variables, in programming languages like Java.
+
+        /*
+         Operators are of five types:
+                                     1. Arithmetic operator---> (+,-,/,*,)
+                                     2. Relational operator---> (<,>,<=,>=,!=,==)
+                                     3. Logical operator---> (&&,||,!)
+                                     4. Increment & Decrement operators---> (++Exp,Exp++), (--Exp,Exp--)
+                                     5. Assignment operator are of two types Simple and Compound
+                                     Simple---> (=), Compound---> (+=,-=,*=,/=)
+         */
+        int num1 = 20;
+        int num2 = 30;
+
+        num1+=10;
+        System.out.println(num1);
+
+        int sum = num1 + num2;
+        System.out.println(sum);
+
+    }
+}
