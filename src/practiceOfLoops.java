ab694c50e73b8a75ed25acc52eb051d4c34097b8
rahul1387 <rahultripathi7447@gmail.com>
2023-05-24 18:47:19 +0530

    Practice of loops and some Questions which are important to know.

---
+++ src/practiceOfLoops.java
@@ -0,0 +1,77 @@
+import java.util.*;
+
+public class practiceOfLoops {
+    public static void main(String[] args) {
+        //Question 1:- Sum of Natural Numbers: Write a program to calculate the sum of the first N natural numbers.
+
+        Scanner check = new Scanner(System.in);
+
+//        System.out.print("Enter a number: ");
+//        int n = check.nextInt();
+//
+//        int sum = 0;
+//        for (int i =1; i<=n; i++){
+//            sum = sum+i;
+//        }
+//        System.out.print("The sum this number is: ");
+//        System.out.println(sum);
+
+        //Question2:- Factorial: Write a program to calculate the factorial of a given number.
+
+//        System.out.print("Enter a number to calculate factorial: ");
+//        int no = check.nextInt();
+//
+//        int multi = 1;
+//        for (int i = 1; i<=no; i++){
+//            multi = multi*i;
+//        }
+//        System.out.print("The Factorial of " + no + " is: ");
+//        System.out.println(multi);
+
+        //Question3:- Fibonacci Series: Write a program to generate the Fibonacci series up to a given limit.
+
+//        int a = 0;
+//        int b = 1;
+//        int c;
+//        System.out.print("Enter a range: ");
+//        int d = check.nextInt();
+//        System.out.println(a + " " + b);
+//
+//        for (int i = 1; i<=d; i++){
+//            c = a+b;
+//            System.out.println(" " + c);
+//            a=b;
+//            b=c;
+//        }
+
+        //Question4:- Prime Numbers: Write a program to check whether a given number is prime or not.
+//                System.out.print("Enter a number: ");
+//                int number = check.nextInt();
+//
+//                boolean isPrime = true;
+//
+//                if (number <= 1)
+//                {
+//                    isPrime = false;
+//                } else
+//                {
+//                    for (int i = 2; i <= Math.sqrt(number); i++)
+//                    {
+//                        if (number % i == 0)
+//                        {
+//                            isPrime = false;
+//                            break;
+//                        }
+//                    }
+//                }
+//                if (isPrime)
+//                {
+//                    System.out.println(number + " is a prime number .");
+//                }else
+//                {
+//                    System.out.println(number + " is not a prime number.");
+//                }
+
+        //Question5:- Reverse a Number: Write a program to reverse a given number.
+    }
+}
