8b1d2e1cba81d3160f39bfcac9ceaf351289baad
rahul1387 <rahultripathi7447@gmail.com>
2023-05-31 18:38:29 +0530

    While loop examples.

--- src/whileLoopPractice.java
+++ src/whileLoopPractice.java
@@ -12,18 +12,58 @@ public class whileLoopPractice {
 //        }
         //Question2
 
-        System.out.print("Enter a number: ");
-        int tableNum = sc.nextInt();
-        int till = 1;
+//        System.out.print("Enter a number: ");
+//        int tableNum = sc.nextInt();
+//        int till = 1;
+//
+//        while (till<=20)
+//        {
+//            int result;
+//            result = tableNum*till;
+//            System.out.println(tableNum + " * " + till + " = " + result);
+//            till++;
+//        }
 
-        while (till<=20)
+        //Question3
+//        System.out.print("Enter digits: ");
+//        long digit = sc.nextLong();
+//
+//        for (int i = 1; i<=digit; i++)
+//        {
+//            System.out.print(i);
+//            System.out.println(digit);
+//        }
+
+        //Question4
+        System.out.println("Welcome to two digit calculator!!");
+        System.out.print("Enter first number: ");
+        int firstNum = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int secondNum = sc.nextInt();
+
+        System.out.print("Which operator would you like to choose(/,*,-,+,%): ");
+        char operator = sc.next().charAt(0);
+         int result = 0;
+
+        if (operator=='*')
         {
-            int result;
-            result = tableNum*till;
-            System.out.println(tableNum + " * " + till + " = " + result);
-            till++;
+           result = firstNum * secondNum;
+        } else if (operator=='/')
+        {
+            result = firstNum / secondNum;
+        } else if (operator=='+')
+        {
+            result = firstNum + secondNum;
+        } else if (operator=='-')
+        {
+            result = firstNum - secondNum;
+        } else if (operator=='%')
+        {
+            result = firstNum % secondNum;
+        }else
+        {
+            System.out.println("Invalid operator");
         }
-
-
+        System.out.println(firstNum + " " + operator + " " + secondNum + " = " + result);
     }
 }
