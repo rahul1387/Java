f57549dde54832b6ffa3ac2e76c1deee4384f300
rahul1387 <rahultripathi7447@gmail.com>
2023-06-04 17:12:25 +0530

    Various animals voice printing using methods

---
+++ src/Animals.java
@@ -0,0 +1,28 @@
+public class Animals {
+
+    public static void cat()
+    {
+        System.out.println("I am cat Mia-uu!");
+    }
+    public static void dog()
+    {
+        System.out.println("I am dog Bhauu Bhauu!");
+    }
+    public static void pig()
+    {
+        System.out.println("I am pig oink oink!");
+    }
+    public static void horse()
+    {
+        System.out.println("I am horse neigh!");
+    }
+    public static void main(String[] args)
+    {
+        cat();
+        dog();
+        pig();
+        horse();
+    }
+}
+
+
