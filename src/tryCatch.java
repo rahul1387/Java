28e92f156ea7d66a05124d8e72948e52f57d7f09
rahul1387 <rahultripathi7447@gmail.com>
2023-06-06 18:28:06 +0530

    try-catch in java.

--- src/tryCatch.java
+++ src/tryCatch.java
@@ -5,11 +5,25 @@ public class tryCatch {
     {
         Scanner sc = new Scanner(System.in);
         //try will, try do the code and catch will store the error in variable Exception ignored
-     try {
-         int num1 = sc.nextInt();
-         int num2 = sc.nextInt();
-         int sum = num1+num2;
-         System.out.println("The sum these two numbers is: " + sum);
-     }catch (Exception ignored){}
+        int a = 0;
+        int b = 1;
+        int c;
+        try
+            {
+                System.out.print("Enter a range: ");
+                int range = sc.nextInt();
+                System.out.println(a + " " + b);
+                for (int i = 1; i<=range; i++)
+                {
+                    c=a+b;
+                    System.out.println(" " + c);
+                    a=b;
+                    b=c; //This is known as swapping method
+                }
+            } catch (Exception ex)
+            {
+                System.out.print("Pls enter only number and try again!!");
+            }
+
     }
 }
