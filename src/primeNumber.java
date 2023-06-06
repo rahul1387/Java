27827564c9bed6536d2ccbd39fc46f6e87c65926
rahul1387 <rahultripathi7447@gmail.com>
2023-06-06 14:46:49 +0530

    try-catch in java.

--- src/primeNumber.java
+++ src/primeNumber.java
@@ -8,31 +8,29 @@ public class primeNumber {
         boolean isPrime = true;
 
         int exit = 1;
-        while (exit==1)
-        {
+        while (exit==1) {
             System.out.print("Enter a number: ");
-            int number = sc.nextInt();
 
-            for (int i = 2; i < number; i++)
-            {
-                if (number % i == 0)
-                {
-                    System.out.println(number + " is not a prime number");
-                    isPrime = false;
-                    break;
+                int number = sc.nextInt();
+
+
+                for (int i = 2; i < number; i++) {
+                    if (number % i == 0) {
+                        System.out.println(number + " is not a prime number");
+                        isPrime = false;
+                        break;
+                    }
                 }
-            }
-            if (number < 0)
-            {
-                System.out.println("Negative number cannot be a prime number!!");
-                isPrime = false;
-            }
-            if (isPrime)
-            {
-                System.out.println(number + " is a prime number");
-            }
-            System.out.println("Do want to continue press(1) if no press(0)");
-            exit = sc.nextInt();
+                if (number < 0) {
+                    System.out.println("Negative number cannot be a prime number!!");
+                    isPrime = false;
+                }
+                if (isPrime) {
+                    System.out.println(number + " is a prime number");
+                }
+                System.out.println("Do want to continue press(1) if no press(0)");
+                exit = sc.nextInt();
+
         }
     }
 }
