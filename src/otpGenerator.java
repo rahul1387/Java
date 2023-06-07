e170e07dddc81f68090367db649a9679a49fdb54
rahul1387 <rahultripathi7447@gmail.com>
2023-06-07 14:47:21 +0530

    This will generate random otp from the given range of user.

---
+++ src/otpGenerator.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class otpGenerator {
+
+    public static void main(String[] args)
+    {
+        double otp = Math.random();
+        int zeroGenerator = ZeroGenerator();
+        System.out.println("Your Otp is: " + (int) (otp * zeroGenerator));
+    }
+    public static int ZeroGenerator()
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a range of otp: ");
+        int rangeOtp = scanner.nextInt();
+        String one = "1";
+
+        for (int i = 0; i<rangeOtp; i++)
+        {
+            one = one + "0" ;
+
+        }
+        // return will return it into calling method means in main method.
+        return (Integer.parseInt(one));
+    }
+}
