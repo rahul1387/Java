f8bbc7e7bb929489aea8f5cd6704808cafef688f
rahul1387 <rahultripathi7447@gmail.com>
2023-10-29 10:53:22 +0530

    Is leap year program.

--- src/leapYear.java
+++ src/leapYear.java
@@ -1,18 +1,26 @@
 import java.util.Scanner;
 
-public class leapYear {
+public class leapYear
+{
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a year: ");
-        int year = sc.nextInt();
+        int exit = 1;
 
-        if (isLeapYear(year))
-        {
-            System.out.println(year + " is a leap year");
-        }else
-        {
-            System.out.println(year + " is not a leap year");
+
+
+        while(exit==1){
+            System.out.print("Enter a year: ");
+            int year = sc.nextInt();
+            if (isLeapYear(year))
+            {
+                System.out.println(year + " is a leap year");
+            }else
+            {
+                System.out.println(year + " is not a leap year");
+            }
+            System.out.print("Do want to continue press(1) if no press(0): ");
+            exit = sc.nextInt();
         }
     }
     public static boolean isLeapYear(int year)
